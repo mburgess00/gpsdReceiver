c631f9cdb53230309c8d8bbf5c3bd12c07b7a2ee
Matt Burgess <matt.burgess@hillmangroup.com>
2022-12-14 15:21:08 -0700

    tweaks

--- gpsdReceiver.sh
+++ gpsdReceiver.sh
@@ -3,8 +3,15 @@
 # get GPS from router
 #
 
-/usr/bin/socat exec:"/data/gpsdReceiver/ext/gpsd/clients/gpspipe -r 172.16.42.3" pty,link=/dev/ttygps,raw,nonblock,echo=0,b115200 &
-while [ `ps|grep gps_dbus|grep -v grep|wc -l` -eq 0 ]; do
-  sleep 1
-  /opt/victronenergy/gps-dbus/gps_dbus -s /dev/ttygps -b 115200 -t 0 &
+while true; do
+
+  while [ `ps|grep gpspipe|grep -v grep|wc -l` -eq 0 ]; do
+    sleep 1
+    /usr/bin/socat exec:"/data/gpsdReceiver/ext/gpsd/clients/gpspipe -r 172.16.42.3" pty,link=/dev/ttygps,raw,nonblock,echo=0,b115200 &
+    done
+  while [ `ps|grep gps_dbus|grep -v grep|wc -l` -eq 0 ]; do
+    sleep 1
+    /opt/victronenergy/gps-dbus/gps_dbus -s /dev/ttygps -b 115200 -t 0 &
+  done
 done
+
